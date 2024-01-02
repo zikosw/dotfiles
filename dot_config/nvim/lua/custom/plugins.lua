258acdc6b1f2b633a513a9cba08cbf505d8dcd03
Supakorn Warodon <supakorn@jigko.net>
2024-01-02 17:12:18 +0700

    add neovim sattelite and wakatime

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,4 @@
 local overrides = require("custom.configs.overrides")
-
 ---@type NvPluginSpec[]
 local plugins = {
 
@@ -206,6 +205,75 @@ local plugins = {
     'f-person/git-blame.nvim',
     event = "VeryLazy",
   },
+  {
+    'lewis6991/satellite.nvim',
+    enabled = false, -- need neovim 0.10+
+    event = "VeryLazy",
+    config = function()
+      require('satellite').setup {
+        current_only = false,
+        winblend = 50,
+        zindex = 40,
+        excluded_filetypes = {},
+        width = 2,
+        handlers = {
+          cursor = {
+            enable = true,
+            -- Supports any number of symbols
+            symbols = { '⎺', '⎻', '⎼', '⎽' }
+            -- symbols = { '⎻', '⎼' }
+            -- Highlights:
+            -- - SatelliteCursor (default links to NonText
+          },
+          search = {
+            enable = true,
+            -- Highlights:
+            -- - SatelliteSearch (default links to Search)
+            -- - SatelliteSearchCurrent (default links to SearchCurrent)
+          },
+          diagnostic = {
+            enable = true,
+            signs = {'-', '=', '≡'},
+            min_severity = vim.diagnostic.severity.HINT,
+            -- Highlights:
+            -- - SatelliteDiagnosticError (default links to DiagnosticError)
+            -- - SatelliteDiagnosticWarn (default links to DiagnosticWarn)
+            -- - SatelliteDiagnosticInfo (default links to DiagnosticInfo)
+            -- - SatelliteDiagnosticHint (default links to DiagnosticHint)
+          },
+          gitsigns = {
+            enable = true,
+            signs = { -- can only be a single character (multibyte is okay)
+              add = "│",
+              change = "│",
+              delete = "-",
+            },
+            -- Highlights:
+            -- SatelliteGitSignsAdd (default links to GitSignsAdd)
+            -- SatelliteGitSignsChange (default links to GitSignsChange)
+            -- SatelliteGitSignsDelete (default links to GitSignsDelete)
+          },
+          marks = {
+            enable = true,
+            show_builtins = false, -- shows the builtin marks like [ ] < >
+            key = 'm'
+            -- Highlights:
+            -- SatelliteMark (default links to Normal)
+          },
+          quickfix = {
+            signs = { '-', '=', '≡' },
+            -- Highlights:
+            -- SatelliteQuickfix (default links to WarningMsg)
+          }
+        },
+      }
+    end
+  },
+  {
+    "wakatime/vim-wakatime",
+    event = "VeryLazy",
+  }
+
 }
 
 return plugins
